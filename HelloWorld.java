abc26ba863fae9f10c4e54a50ba5686669788b67
Manjunatha T <71175869+Manju1043@users.noreply.github.com>
2023-03-09 16:18:25 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+    sc.nextLine();
+    System.out.println("Hello World");
+    sc.close();
+	}
+
+}
